9755cce041bda5b72a33d91b8d0757ff49f614f4
theycallmemac <james.mcdermott89@gmail.com>
2017-12-11 14:16:39 +0000

    created setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+setup(name='dcurooms',
+      version='0.1.0',
+      description='Shows room information around the DCU campus',
+      author='theycallmemac',
+      url='',
+      license = 'MIT',
+      scripts=['scripts/dcurooms'],
+      install_requires=[
+          'mechanize', 'bs4', 'requests'
+      ],
+      classifiers=[
+            'Environment :: Console',
+            'Natural Language :: English',
+            'License :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python'
+])
