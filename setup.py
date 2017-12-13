b5432f958f273381dc4a3b48f3b01c2b00b5655d
theycallmemac <james.mcdermott89@gmail.com>
2017-12-13 18:10:45 +0000

    changes made. now at v0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 from setuptools import find_packages
 setup(name='dcurooms',
-      version='0.1.0',
+      version='0.2.0',
       description='Shows room information around the DCU campus',
       author='theycallmemac',
       url='',
