1526f19b3e72b96540507c2d9f4cbb6d1ec6cb18
James Mc Dermott <james.mcdermott7@mail.dcu.ie>
2018-01-13 03:09:30 +0000

    Update controls.py

--- scripts/controls.py
+++ scripts/controls.py
@@ -27,7 +27,8 @@ def run_loop(lst, options, details):
     for room in lst:
         timetable, url = builders.build_timetable("GLA." + room, week, day, time)
         status = checks.check_room(url)
-        print(room + ": " + status) if options.available and len(status) <= 9 else print(room + ": " + status)
+        if options.available and len(status) <= 9: print(room + ": " + status)
+        else: print(room + ": " + status)
 
 
 def booking_control(c, g, details):
